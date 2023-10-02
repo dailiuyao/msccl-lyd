928f1053d3d7d99d67ecf2ac070448b60629ccd2
dailiuyao <ldai8@ucmerced.edu>
2023-10-02 01:20:33 -0700

    force msccl to choose msccl algo

--- src/graph/tuning.cc
+++ src/graph/tuning.cc
@@ -395,8 +395,16 @@ ncclResult_t ncclTopoGetMSCCLAlgo(struct ncclInfo* info) {
           info->protocol = mscclAlgo->protocol;
           info->mscclInfo.mscclAlgoIndex = i;
           // printf("\n[TopoGet2_LYD INFO]info->algorithm is: %d\n", info->algorithm);
+          // printf("\n[TopoGet2_LYD INFO]info->mscclInfo.mscclAlgoIndex is: %d\n", info->mscclInfo.mscclAlgoIndex);
           return ncclSuccess;
         }
+        // force algo to msccl by LYD
+        // if ((mscclAlgo->isValid) && (inPlace == mscclAlgo->inPlace)) {
+        info->algorithm = NCCL_ALGO_MSCCL;
+        info->protocol = mscclAlgo->protocol;
+        info->mscclInfo.mscclAlgoIndex = i; 
+        return ncclSuccess; 
+        // }
       }
     }
   }
